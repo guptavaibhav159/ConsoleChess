81951782b8f00a6c4fa654ab4f65526e0cb56293
C4tch0603 <e.miniberger@gmail.com>
2020-09-30 14:49:05 +0200

    Modified ChessPiece class

--- ConsoleChess/ConsoleChess/ChessPiece.cs
+++ ConsoleChess/ConsoleChess/ChessPiece.cs
@@ -1,7 +1,19 @@
 namespace ConsoleChess
 {
-    public interface ChessPiece
+    public abstract class ChessPiece
     {
-        
+        public bool IsWhite { get; }
+        public (int, int) Position { get; }
+        public int[] RelativeMovement { get; set; }
+        public char Symbol { get; }
+
+        public ChessPiece(bool isWhite, (int, int) position, char symbol)
+        {
+            IsWhite = isWhite;
+            Position = position;
+            Symbol = symbol;
+        }
+
+        public abstract void CalculateRelativeMovement();
     }
 }
